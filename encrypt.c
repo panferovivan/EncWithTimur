6c59e6e3b1d6ff02a92eb9d0d25a7bdd618bf25b
panferovivan <panferovivan89stu@gmail.com>
2025-10-26 01:37:04 +0700

    kost

---
+++ encrypt.c
@@ -0,0 +1,46 @@
+#include "encrypt.h"
+
+void encrypt_meccage(const char *plan_text, const char *output_file, const char password)
+{
+    printf("функция шифрования \n");
+}
+
+void decrypt_and_display(const char *input_file, const char *password)
+{
+    printf("функиця дешифрования \n");
+}
+
+unit32_t generate_seed(const char *password)
+{
+    return 0;
+}
+viod input_key_gen(unit32_t seed)
+{
+
+}
+
+unsigned char get_next_key_byte()
+{
+    
+}
+
+viod crypto_operation(FILE *input_file, FILE *output_file, const char *password)
+{
+
+}
+
+void clear_memory(void *ptr, size_t size)
+{
+
+}
+
+int main()
+{
+
+    printf("====CRYPRO CHEWBACCA==== \n");
+    printf("1. Зашифровать сообщение \n");
+    printf("2. Расшифровать сообщение \n");
+    printf("3. Досвидоний... \n");
+    printf("Выбери действие: ");
+    return 0;
+}
--- encrypt.h
+++ encrypt.h
@@ -2,10 +2,12 @@
 #define ENCRYPT_H
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 unit32_t generate_seed(const char *password);
 viod input_key_gen(unit32_t seed);
 unsigned char get_next_key_byte();
+void encrypt_meccage(const char *plan_text, const char *output_file, const char password);
 viod crypto_operation(FILE *input_file, FILE *output_file, const char *password);
 void decrypt_and_display(const char *input_file, const char *password);
 void clear_memory(void *ptr, size_t size);
