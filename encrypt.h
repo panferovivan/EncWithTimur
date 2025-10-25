9968c04e1816281f31da8d46d5bdbcb57fa8a53e
panferovivan <panferovivan89stu@gmail.com>
2025-10-26 00:34:20 +0700

    nachalo2

---
+++ encrypt.h
@@ -0,0 +1,14 @@
+#ifndef ENCRYPT_H
+#define ENCRYPT_H
+#include <stdio.h>
+#include <string.h>
+
+unit32_t generate_seed(const char *password);
+viod input_key_gen(unit32_t seed);
+unsigned char get_next_key_byte();
+viod crypto_operation(FILE *input_file, FILE *output_file, const char *password);
+void decrypt_and_display(const char *input_file, const char *password);
+void clear_memory(void *ptr, size_t size);
+
+
+#endif
