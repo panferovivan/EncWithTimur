a85a7436c0e61b52b54e3c1a97528399d927ec41
panferovivan <panferovivan89stu@gmail.com>
2025-10-28 15:06:38 +0700

    281025_2

--- encrypt.h
+++ encrypt.h
@@ -11,10 +11,10 @@ typedef struct
 
 
 unit32_t generate_seed(const char *password);
-viod init_key_gen(crypto_ctx_t *ctx, unit32_t seed);
+void init_key_gen(crypto_ctx_t *ctx, unit32_t seed);
 unsigned char get_next_key_byte(crypto_ctx_t *ctx);
 void encrypt_meccage(const char *plan_text, const char *output_file, const char *password);
-viod crypto_operation(FILE *input_file, FILE *output_file, const char *password);
+void crypto_operation(FILE *input_file, FILE *output_file, const char *password);
 void decrypt_and_display(const char *input_file, const char *password);
 void clear_memory(void *ptr, size_t size);
 
